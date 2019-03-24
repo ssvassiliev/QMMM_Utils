09b3debbde2a4e55d73368446579a8e0413b9007
svassili <svassiliev1@brocku.ca>
2019-03-24 18:18:35 -0300

    amber ff fixes

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 - Prepare oniom input file from AMBER parm7 and rst7 files.
 - Extract coordinates from gaussian optimization log in AMBER rst7 format. 
 - Convert AMBER force fields to GAUSSIAN
+- ff14SB contains atom types 2C and 3C. Gaussian does not recognize types which start with a number. Workaround: `preponiom` renames 2C, 3C with Z2, Z3.
+- ff14 has C\* and N\* entries in VDW table. Gaussian does not recognize wildcards, so all types like CA, CB .. etc must be manuall added to gaussian prm file.
--- prep_oniom/prmtop2oniom.c
+++ prep_oniom/prmtop2oniom.c
@@ -21,7 +21,7 @@ void read_freeze(char *restr, int *at_freeze, char *at_level, int *link)
 {
   FILE *fp;
   char line_buf[200];
-  char aname[5], resname[5];  
+  char aname[5], resname[5];
   int i,j,k,start,stop,nrest,nlink,bnd2;
   char bondedTo[5], lAtm[5], lTyp[5];
 
@@ -41,14 +41,14 @@ void read_freeze(char *restr, int *at_freeze, char *at_level, int *link)
   for(i=0;i<N;i++)
     {
       h_num[i]=0;
-      // BONDS WITH HYDROGEN     
+      // BONDS WITH HYDROGEN
       for(j=0;j<NBONH;j++)
 	{
 	  if(IBH[j]==i)
 	    h_list[i][h_num[i]++]=JBH[j];
 	  if(JBH[j]==i)
 	    h_list[i][h_num[i]++]=IBH[j];
-	}  
+	}
     }
 
   prmcnt=0;
@@ -56,7 +56,7 @@ void read_freeze(char *restr, int *at_freeze, char *at_level, int *link)
     {
       if(fgets(line_buf,82,fp)==NULL)
 	break;
-      nrest=0;nlink=0;     
+      nrest=0;nlink=0;
       /*-------------------------FF PARAMETER FILES------------------------------*/
       if(!strncmp(line_buf,"PARAMS",6))
 	{ sscanf(&line_buf[7], "%s", &params[prmcnt++]);continue;}
@@ -77,7 +77,7 @@ void read_freeze(char *restr, int *at_freeze, char *at_level, int *link)
 	    if((i>=start-1)&&(i<=stop-1))
 	      {nrest++; at_level[i]=level;}
 	  printf("Model level  %c set for %6i atoms: index %i-%i\n", level,nrest,start,stop);continue;
-	}   
+	}
       if(!strncmp(line_buf,"LEVEL INDEXH",12))
 	{
 	  sscanf(&line_buf[20], "%i%i %c", &start, &stop, &level);
@@ -89,7 +89,7 @@ void read_freeze(char *restr, int *at_freeze, char *at_level, int *link)
 		  at_level[h_list[i][k]]=level;
 	      }
 	  printf("Model level  %c set for %6i atoms: index %i-%i\n", level,nrest,start,stop);continue;
-	}   
+	}
       /*-------------------------------------------------------------*/
       if(!strncmp(line_buf,"LEVEL RESID ",12))
 	{
@@ -98,7 +98,7 @@ void read_freeze(char *restr, int *at_freeze, char *at_level, int *link)
 	    if((resSeq[i]>=start-1)&&(resSeq[i]<=stop-1))
 	      {nrest++; at_level[i]=level;}
 	  printf("Model level  %c set for %6i atoms: resid %i-%i\n", level,nrest,start,stop);continue;
-	}   
+	}
       /*-------------------------------------------------------------*/
       if(!strncmp(line_buf,"LEVEL ANAME ",12))
 	{
@@ -126,7 +126,7 @@ void read_freeze(char *restr, int *at_freeze, char *at_level, int *link)
 	      if(!strncmp(RESIDUE_LABEL[j],resname,strlen(resname)))
 		for(i=RESIDUE_POINTER[j]-1+start-1;i<=RESIDUE_POINTER[j]-1+stop-1;i++)
 		  {
-		    nrest++; 
+		    nrest++;
 		    at_level[i]=level;
 		  }
 	    }
@@ -144,7 +144,7 @@ void read_freeze(char *restr, int *at_freeze, char *at_level, int *link)
 		for(i=RESIDUE_POINTER[j]-1;i<RESIDUE_POINTER[j+1]-1;i++)
 		  if(!strncmp(&atom_name[i][0],aname,strlen(aname))&&(strlen(aname)==strlen(&atom_name[i][0])))
 		    {
-		      nrest++; 
+		      nrest++;
 		      at_level[i]=level;
 		      for(k=0;k<h_num[i];k++)
 			at_level[h_list[i][k]]=level;
@@ -162,7 +162,7 @@ void read_freeze(char *restr, int *at_freeze, char *at_level, int *link)
 	      {
 		if(!strncmp(&atom_name[i][0],aname,strlen(aname))&&(strlen(aname)==strlen(&atom_name[i][0])))
 		  {
-		    nrest++; 
+		    nrest++;
 		    at_level[i]=level;
 		      for(k=0;k<h_num[i];k++)
 			at_level[h_list[i][k]]=level;
@@ -187,12 +187,12 @@ void read_freeze(char *restr, int *at_freeze, char *at_level, int *link)
 		for(i=RESIDUE_POINTER[j]-1;i<RESIDUE_POINTER[j+1]-1;i++)
 		    if(!strncmp(&atom_name[i][0], aname, strlen(aname))&&(strlen(aname)==strlen(&atom_name[i][0])))
 		      {
-			nlink++; 
+			nlink++;
 			link[i]=bnd2;
 			strcpy(&linkAtom[i][0],lAtm);
 			strcpy(&linkType[i][0],lTyp);
-		      }		
-		}  
+		      }
+		}
 	    }
 	  printf("Link %s %s-%s:%i\n", resname, aname, bondedTo,  nlink);continue;
 	}
@@ -210,12 +210,12 @@ void read_freeze(char *restr, int *at_freeze, char *at_level, int *link)
 		for(i=RESIDUE_POINTER[j]-1;i<RESIDUE_POINTER[j+1]-1;i++)
 		    if(!strncmp(&atom_name[i][0], aname, strlen(aname))&&(strlen(aname)==strlen(&atom_name[i][0])))
 		      {
-			nlink++; 
+			nlink++;
 			link[i]=bnd2;
 			strcpy(&linkAtom[i][0],lAtm);
 			strcpy(&linkType[i][0],lTyp);
-		      }		
-		}  
+		      }
+		}
 	    }
 	  printf("Link resid %i-%i %s-%s:%i\n", start,stop, aname, bondedTo,  nlink);continue;
 	}
@@ -227,11 +227,11 @@ void read_freeze(char *restr, int *at_freeze, char *at_level, int *link)
 	  for(i=RESIDUE_POINTER[j]-1;i<RESIDUE_POINTER[j+1]-1;i++)
 	    if(!strncmp(&atom_name[i][0], aname, strlen(aname))&&(strlen(aname)==strlen(&atom_name[i][0])))
 	      {
-		nlink++; 
+		nlink++;
 		link[i]=bnd2;
 		strcpy(&linkAtom[i][0],lAtm);
 		strcpy(&linkType[i][0],lTyp);
-	      }		
+	      }
 	  printf("Link resid %i %s-%i:%i\n", start, aname, bnd2,  nlink);continue;
 	}
 
@@ -301,9 +301,9 @@ void read_freeze(char *restr, int *at_freeze, char *at_level, int *link)
 	    {
 	      if(!strncmp(RESIDUE_LABEL[j],resname,strlen(resname)))
 		for(i=RESIDUE_POINTER[j];i<=RESIDUE_POINTER[j+1]-1;i++)
-		  if(!strncmp(&atom_name[i][0],aname,strlen(aname))&&(strlen(aname)==strlen(&atom_name[i][0])))	
+		  if(!strncmp(&atom_name[i][0],aname,strlen(aname))&&(strlen(aname)==strlen(&atom_name[i][0])))
 		    {
-		      nrest++; 
+		      nrest++;
 		      at_freeze[i]=cf;
 		      for(k=0;k<h_num[i];k++)
 			at_freeze[h_list[i][k]]=cf;
@@ -321,7 +321,7 @@ void read_freeze(char *restr, int *at_freeze, char *at_level, int *link)
 	      {
 		if(!strncmp(&atom_name[i][0],aname,strlen(aname))&&(strlen(aname)==strlen(&atom_name[i][0])))
 		  {
-		    nrest++; 
+		    nrest++;
 		    at_freeze[i]=cf;
 		      for(k=0;k<h_num[i];k++)
 			at_freeze[h_list[i][k]]=cf;
@@ -330,7 +330,7 @@ void read_freeze(char *restr, int *at_freeze, char *at_level, int *link)
       printf("Freeze type %2i set for %6i atoms: resid %i-%i/%s\n",cf,nrest,start,stop, aname);
       continue;
 	}
-  
+
   if(strlen(line_buf)>5)
     printf("Unknown keyword: %s",line_buf);
 }
@@ -351,10 +351,10 @@ void write_oniom(char *restr, char *label)
   int bnd2;
   char tmpstr[100];
 
-  
+
   chrg=0;
   chrgf=0;
-  
+
   at_freeze=malloc(N*sizeof(int));
   at_level=malloc(N*sizeof(char));
   link=malloc(N*sizeof(int));
@@ -366,11 +366,11 @@ void write_oniom(char *restr, char *label)
 
   mass_to_element();
   read_freeze(restr, at_freeze, at_level, link);
- 
+
 
   // MAKE LISTS OF BONDED ATOMS
   for(i=0;i<N;i++)
-    {// read only 3 digits in charges  
+    {// read only 3 digits in charges
       chrg+=nearbyint(charge[i]*1000)/1000;
       chrgf+=charge[i];
       connect_num[i]=0;
@@ -378,18 +378,18 @@ void write_oniom(char *restr, char *label)
       for(j=0;j<NBONA;j++)
 	{
 	  if(IB[j]==i)
-	    connect_list[i][connect_num[i]++]=JB[j];  
+	    connect_list[i][connect_num[i]++]=JB[j];
 	  if(JB[j]==i)
 	    connect_list[i][connect_num[i]++]=IB[j];
 	}
-      // BONDS WITH HYDROGEN     
+      // BONDS WITH HYDROGEN
       for(j=0;j<NBONH;j++)
 	{
 	  if(IBH[j]==i)
 	    connect_list[i][connect_num[i]++]=JBH[j];
 	  if(JBH[j]==i)
 	    connect_list[i][connect_num[i]++]=IBH[j];
-	}  
+	}
     }
 
   D_chrg = nearbyint((chrg-chrgf)*1000);
@@ -397,7 +397,7 @@ void write_oniom(char *restr, char *label)
   if(nearbyint(chrgf*1000)/1000 != 0.0)
     printf("\n ** WARNING! ** Total charge is %.4f\n Check if the charge was intended to be non zero\n\n", chrgf);
   // CORRECT CHARGE
-  
+
   printf("Total/Trunc charge: %.4f/%.3f, Diff: 0.00%i\n",chrgf, chrg, abs(D_chrg) );
   printf("Changing charges on %i atoms by 0.001\n", abs(D_chrg) );
   for(i=0;i<abs(D_chrg);i++)
@@ -410,23 +410,26 @@ void write_oniom(char *restr, char *label)
     chrg+=nearbyint(charge[i]*1000)/1000;
   printf("Corrected charge:   %.4f\n", chrg);
 
- 
+
   fp = fopen(label,"wt");
-  if(fp == NULL){printf("** E ** could not open file %s for writing, abort\n", label); exit(1);}  
+  if(fp == NULL){printf("** E ** could not open file %s for writing, abort\n", label); exit(1);}
   printf("Writing ONIOM input file: %s\n", label);
-  
+
   //Strip spaces
-  for(i=0;i<N;i++)
+  for(i=0;i<N;i++){
       if(Type[i][1]==' ')Type[i][1]='\0';
+      if (!strcmp(Type[i],"2C"))strcpy(Type[i],"Z2");
+      if (!strcmp(Type[i],"3C"))strcpy(Type[i],"Z3");
+      }
   fprintf(fp,"%cmem=320MW\n",'%');
   fprintf(fp,"%cnproc=8\n",'%');
   fprintf(fp,"#T ONIOM(HF/6-31G:Amber=softonly)=EmbedCharge Geom=Connectivity Opt\n\nTitle Card\n\n0 1 0 1 0 1\n");
   for(i=0;i<N;i++)
     {
       sprintf(tmpstr,"%s-%s-%.3f",Element[i],Type[i],nearbyint(charge[i]*1000)/1000);
-      fprintf(fp,"%s",tmpstr);   
+      fprintf(fp,"%s",tmpstr);
       for(j=0;j<12-strlen(tmpstr);j++)
-	fprintf(fp," ");  
+	fprintf(fp," ");
       fprintf(fp,"%3i%11.6f%11.6f%11.6f %c ",at_freeze[i],X[i],Y[i],Z[i],at_level[i]);
       if(link[i])
 	fprintf(fp,"%s-%s-0.1 %i\n",&linkAtom[i][0], &linkType[i][0],link[i]);
@@ -439,10 +442,10 @@ void write_oniom(char *restr, char *label)
   for(i=0;i<N;i++)
     {
       fprintf(fp,"%i ",i+1);
-      
+
       for(j=0;j<connect_num[i];j++)
 	if(connect_list[i][j]>i)
-	  fprintf(fp,"%i 1.0 ",connect_list[i][j]+1);		
+	  fprintf(fp,"%i 1.0 ",connect_list[i][j]+1);
       fprintf(fp,"\n");
     }
   fprintf(fp,"\n");
@@ -458,21 +461,21 @@ void write_oniom(char *restr, char *label)
       chainID[i][0]=at_level[i];
       tempFactor[i]=at_freeze[i];
       sprintf(tmpstr,"%5i %s-%s-%.3f",i,Element[i],Type[i],nearbyint(charge[i]*1000)/1000);
-      fprintf(fp,"%s",tmpstr);   
+      fprintf(fp,"%s",tmpstr);
       for(j=0;j<18-strlen(tmpstr);j++)
-	fprintf(fp," ");  
+	fprintf(fp," ");
       fprintf(fp,"%3i%11.6f%11.6f%11.6f %c ",at_freeze[i],X[i],Y[i],Z[i],at_level[i]);
       if(link[i])
 	{fprintf(fp,"%s-%s-0.1 %i\n",&linkAtom[i][0], &linkType[i][0],link[i]); chainID[i][0]='Q';}
       else
 	fprintf(fp,"\n");
     }
-  fclose(fp); 
+  fclose(fp);
 
  for(i=0;i<N;i++)
    if(link[i])
      chainID[link[i]-1][0]='q';
-     
+
   free(at_freeze);
   free(at_level);
   free(link);
@@ -481,7 +484,7 @@ void write_oniom(char *restr, char *label)
 
 
 int main(int argc, char **argv)
-{  
+{
   int c;
   char usage[]="\n"
 
@@ -507,11 +510,11 @@ int main(int argc, char **argv)
     "FREEZE ALL          -1|0\n"
     "FREEZE INDEX        1 2 -1|0\n"
     "FREEZE INDEXH       1 2 -1|0\n"
-    "FREEZE RESID        1 2 -1|0\n" 
+    "FREEZE RESID        1 2 -1|0\n"
     "FREEZE ANAME        MG -1|0\n"
     "FREEZE RNAME        BCL -1|0\n"
-    "FREEZE RNAME:ANAME  BCL CA -1|0\n" 
-    "FREEZE RESID:ANAME  1 2 CA -1|0\n" 
+    "FREEZE RNAME:ANAME  BCL CA -1|0\n"
+    "FREEZE RESID:ANAME  1 2 CA -1|0\n"
     "LINK RNAME:ANAME    U10 C7 H HC C8\n"
     "LINK RESID:ANAME    1 10 CB H HC CA\n"
     "LINK RESID:INDEX    1 CB H HC 1221\n"
@@ -524,20 +527,20 @@ int main(int argc, char **argv)
   char *restr = "const.in";
 
   opterr=0;
-  while( (c=getopt(argc,argv,"p:c:r:o:h")) != -1) 
+  while( (c=getopt(argc,argv,"p:c:r:o:h")) != -1)
     {
-      switch(c) 
+      switch(c)
 	{
-	case 'o': 
+	case 'o':
 	  oniom=optarg;
 	  break;
-	case 'p': 
+	case 'p':
 	  prmtop=optarg;
 	  break;
-	case 'c': 
+	case 'c':
 	  coord=optarg;
 	  break;
-	case 'r': 
+	case 'r':
 	  restr=optarg;
 	  break;
 	case 'h':
@@ -545,8 +548,8 @@ int main(int argc, char **argv)
 	  abort();
 	}
     }
-  
-  if(!read_parm7(prmtop))
+
+  if(read_parm7(prmtop))
     exit(0);
 
   read_amber_coor(coord, N, X, Y, Z);
@@ -556,4 +559,3 @@ int main(int argc, char **argv)
 
   return(1);
 }
-
